cd601103b6933a67a42d46b85f1e4ac684d6f62a
ZameerAbbas <meloohunzai@gmail.com>
2024-08-25 16:09:20 +0500

    user auth

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserData } from "../../redux/slice/authSlice";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const userdata = useSelector((state) => state.auth.userData);
+  useEffect(() => {
+    dispatch(fetchUserData({}));
+  }, [dispatch]);
+  console.log("userdata", userdata);
   return (
     <div>
       <h1>Welcome to the Protected Home Page!</h1>
       <p>This content is only accessible by logged-in users.</p>
+      <div>
+      <h1>{userdata.firstName}</h1>
+      <img src={userdata.image} alt={userdata.firstName} />
+      </div>
     </div>
   );
 };
--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -8,15 +8,31 @@ export const loginApi = createAsyncThunk("auth/login", async (credentials) => {
       "Content-Type": "application/json",
     },
   });
+  localStorage.setItem('token', res.data.token);
   return res.data; 
 });
 
+const token1 = localStorage.getItem('token');
+export const fetchUserData = createAsyncThunk(
+    "auth/fetchUserData",
+    async () => {
+      const res = await axios.get("https://dummyjson.com/auth/me", {
+        headers: {
+          'Authorization': `Bearer ${token1}`,
+        },
+      });
+      return res.data;
+    }
+  );
+
 const authSlice = createSlice({
-  name: "Authlogin",
+  name: "auth",
   initialState: {
     isLoading: false,
     loginData: null, 
     loginError: null,
+    userData: null, 
+    userDataError: null,
   },
   extraReducers: (builder) => {
     
@@ -26,11 +42,23 @@ const authSlice = createSlice({
     builder.addCase(loginApi.fulfilled, (state, action) => {
       state.isLoading = false;
       state.loginData = action.payload;
+      localStorage.setItem('token', action.payload.token);
     });
     builder.addCase(loginApi.rejected, (state, action) => {
       state.isLoading = false;
       state.loginError = action.error.message;
     });
+    builder.addCase(fetchUserData.pending, (state) => {
+        state.isLoading = true;
+      });
+      builder.addCase(fetchUserData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userData = action.payload;
+      });
+      builder.addCase(fetchUserData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.userDataError = action.error.message;
+      });
   },
 });
 
